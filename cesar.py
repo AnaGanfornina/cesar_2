49c8527690603e8badc5fde84c4658cf44d3db59
Ana Ganfornina <anies.llamani@gmail.com>
2024-12-08 21:09:30 +0100

    Terminado los test de función encrypt_word, que encripta palabras

--- cesar.py
+++ cesar.py
@@ -1,21 +1,46 @@
-def cifrador(letra:str,num:int)->str:
+def encrypt(letter:str,num:int)->str:
     """
     Recibe una letra y un número, devuelve la letra del diccionario que corresponda
     al número de saltos indicados
     """
-    dictionary = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-                   "m", "n","ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    dictionary = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+                   "K", "L", "M", "N","Ñ", "O", "P", "Q", "R", "S", "T",
+                     "U", "V", "W", "X", "Y", "Z"," "]
+
     
     encrypted = ""
+    
     for index,item in enumerate(dictionary):
-        if item == letra:
-            encrypted = dictionary[index + num]
-            break
+        if item == letter.upper():
+            if index + num >= len(dictionary):
+                encrypted = dictionary[(index + num)-len(dictionary)]
+                break
+            else:
+                encrypted = dictionary[index + num]
+                break
 
     return encrypted
 
-
-assert cifrador("a",2) == "c"
+def encrypt_word(word:str,num:int)->str:
+    """
+    Recibe una palabra y un número, devuelve la palabra dcifrada
+    """
+    new_word = ""
+    for leter in word:
+        new_word += encrypt(leter,num)
+    return new_word
 
 def creaCifrador(d:int)->callable:
-    pass
+    return encrypt(d)
+
+assert encrypt("a",2) == "C"
+assert encrypt("C",2) == "E"
+assert encrypt("Z",2) == "A"
+assert encrypt("z",2) == "A"
+assert encrypt(" ",2) == "B"
+
+
+assert encrypt_word("CABRA", 2) == "ECDTC", "no funciona"
+assert encrypt_word("ZIGZAG", 2) == "AKIACI", "no funciona"
+assert encrypt_word("ZigZag", 2) == "AKIACI", "no funciona"
+#assert creaCifrador
