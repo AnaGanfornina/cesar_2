e8f212a5ac41a59b2f43a57722f3b369ba2b7e97
Ana Ganfornina <anies.llamani@gmail.com>
2024-12-10 13:30:32 +0100

    fix: corrijo bug out of index de encrypt_letter

--- cesar.py
+++ cesar.py
@@ -1,3 +1,5 @@
+from typing import *
+
 def encrypt_letter(letter:str,num:int)->str:
     """
     Recibe una letra y un número, devuelve la letra del diccionario que corresponda
@@ -12,12 +14,11 @@ def encrypt_letter(letter:str,num:int)->str:
     
     for index,item in enumerate(dictionary):
         if item == letter.upper():
-            if index + num >= len(dictionary):
-                encrypted = dictionary[(index + num)-len(dictionary)]
-                break
-            else:
-                encrypted = dictionary[index + num]
-                break
+            encrypted = dictionary[(index + num) % len(dictionary)] 
+            break
+        else:
+            encrypted = letter
+            
 
     return encrypted
 
@@ -30,7 +31,7 @@ def encrypt_word(word:str,num:int)->str:
         new_word += encrypt_letter(leter,num)
     return new_word
 
-def make_encrypt(d:int)->callable:
+def make_encrypt(d:int)->Callable:
    
    def encrypt(word:str)-> str:
         return encrypt_word(word,d)
@@ -47,25 +48,3 @@ def creaParCesar(d:int):
     
     return encrypt,decrypt
    
-
-assert encrypt_letter("a",2) == "C"
-assert encrypt_letter("C",2) == "E"
-assert encrypt_letter("Z",2) == "A"
-assert encrypt_letter("z",2) == "A"
-assert encrypt_letter(" ",2) == "B"
-
-
-assert encrypt_word("CABRA", 2) == "ECDTC", "no funciona"
-assert encrypt_word("ZIGZAG", 2) == "AKIACI", "no funciona"
-assert encrypt_word("ZigZag", 2) == "AKIACI", "no funciona"
-
-
-cesar2 = make_encrypt(2)
-assert cesar2("ZigZag") == "AKIACI" 
-
-
-cifrar3,desdifrar3 = creaParCesar(3)
-assert cifrar3("ZigZag") == "BLJBDJ"
-assert desdifrar3("BLJBDJ") == "ZIGZAG"
-
-
